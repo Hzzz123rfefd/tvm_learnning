b9e82d5ea206818b0f304f416e27480844f686b9
zhongxiaohui <zhongxiaohui@jiigan.com>
2025-07-14 14:37:29 +0800

    update files

--- inference/inference.cpp
+++ inference/inference.cpp
@@ -41,26 +41,6 @@
         std::cout << std::endl;
     }
 #else
-    TEST(INTERVIT, OPENCL) {
-        ModelBase model("output_pytroch_linux_cpu/output_pytroch_linux_cpu.so", "output_pytroch_linux_cpu/output_pytroch_linux_cpu.bin", "llvm");
-        DLDevice dev{kDLCPU, 0};
-        tvm::runtime::NDArray x_tvm = tvm::runtime::NDArray::Empty({1, 784}, {kDLFloat, 32, 1}, dev);
-        std::fill((float*)x_tvm->data, (float*)x_tvm->data + 784, 1.0f);
-
-        tvm::runtime::NDArray y_tvm = model.inference(x_tvm).CopyTo(dev);
-
-        float* data = static_cast<float*>(y_tvm->data);
-        int64_t total = 1;
-        for (int64_t dim : y_tvm.Shape()) {
-            total *= dim;
-        }
-        std::cout << "output (first 10 values): ";
-        for (int64_t i = 0; i < std::min<int64_t>(10, total); ++i) {
-            std::cout << data[i] << " ";
-        }
-        std::cout << std::endl;
-    }
-    
     TEST(PYTORCH, CUDA) {
         ModelBase model("output_pytroch_linux_cuda/output_pytroch_linux_cuda.so", "output_pytroch_linux_cuda/output_pytroch_linux_cuda.bin", "cuda");
         DLDevice dev{kDLCPU, 0};
